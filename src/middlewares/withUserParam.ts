a63cfdb36f805a6a25a0b4ba03364b4492253ace
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-11 21:39:22 -0300

    Bump cig core (#9)
    * Bump @cig-platform/core to 0.2.25
    
    * Bump @cig-platform/core to 0.2.27
    
    * Refactor withUserParam with withRequestParam
    
    * Delete middleware test

--- src/middlewares/withUserParam.ts
+++ src/middlewares/withUserParam.ts
@@ -1,26 +1,7 @@
-import { NextFunction, Response } from 'express'
-import { ApiError, BaseController, NotFoundError } from '@cig-platform/core'
+import { withRequestParam } from '@cig-platform/core'
 
-import { UserRequest } from '@Types/requests'
 import UserController from '@Controllers/UserController'
 import UserRepository from '@Repositories/UserRepository'
+import User from '@Entities/UserEntity'
 
-export const withUserParamFactory = (errorCallback: (res: Response, error: ApiError) => Response, repository?: UserRepository) => {
-  return async (request: UserRequest, response: Response, next: NextFunction): Promise<void | Response<string, Record<string, string>>> => {
-    const userId = request?.params?.userId
-
-    const userRepository = repository ?? UserController.repository
-
-    return userRepository.findById(userId)
-      .then(user => {
-        if (!user) throw new NotFoundError()
-
-        request.user = user
-
-        next()
-      })
-      .catch((error) => errorCallback(response, error?.getError?.() ?? error))
-  }
-}
-
-export default withUserParamFactory(BaseController.errorResponse)
+export default withRequestParam<UserRepository, User>('userId', 'user', UserController)
