c5d9555157dd35a2607e863d94d1105b05069caa
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-05 20:37:01 -0300

    Replace EncryptService implementation for lib

--- src/services/EncryptService.ts
+++ src/services/EncryptService.ts
@@ -1,20 +1,5 @@
-import CryptoJS from 'crypto-js'
+import EncryptService from '@cig-platform/encrypt'
 
 import { ENCRYPT_KEY } from '@Constants/encrypt'
 
-export default class EncryptService {
-  static encrypt(originalString: string) {
-    return CryptoJS.AES.encrypt(originalString, ENCRYPT_KEY).toString()
-  }
-
-  static decrypt(encryptedString: string) {
-    const bytes  = CryptoJS.AES.decrypt(encryptedString, ENCRYPT_KEY)
-    const originalText = bytes.toString(CryptoJS.enc.Utf8)
-
-    return originalText
-  }
-
-  static check(originalString: string, encryptedString: string) {
-    return originalString === EncryptService.decrypt(encryptedString)
-  }
-}
+export default new EncryptService(ENCRYPT_KEY)
