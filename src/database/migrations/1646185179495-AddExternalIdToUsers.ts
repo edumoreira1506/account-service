9efbed786aada4a89eef56d56abc8af68e4c1d11
Eduardo Moreira <contato@eduardoem.com.br>
2022-03-01 22:44:57 -0300

    Fix migration

--- src/database/migrations/1646185179495-AddExternalIdToUsers.ts
+++ src/database/migrations/1646185179495-AddExternalIdToUsers.ts
@@ -4,7 +4,7 @@ export class AddExternalIdToUsers1646185179495 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.addColumn('users', new TableColumn({
       name: 'external_id',
-      type: 'uuid',
+      type: 'varchar',
       isNullable: true,
     }))
   }
