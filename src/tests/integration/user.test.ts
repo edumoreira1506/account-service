21194f6a2d00bc8eb7f23dbf767995ed291ea676
Eduardo Moreira <contato@eduardoem.com.br>
2022-03-06 13:00:22 -0300

    Replace legacy faker-js for new faker js

--- src/tests/integration/user.test.ts
+++ src/tests/integration/user.test.ts
@@ -1,6 +1,6 @@
 import request from 'supertest'
 import typeorm from 'typeorm'
-import faker from 'faker'
+import faker from '@faker-js/faker'
 import { userFactory } from '@cig-platform/factories'
 
 import App from '@Configs/server'
