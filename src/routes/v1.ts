a14077e8ced314c7af5f2b661df9579f00517918
edumoreira1506 <contato@eduardoem.com.br>
2022-09-09 23:59:32 -0300

    Add api key validation

--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -4,16 +4,17 @@ import { withBodyValidation } from '@cig-platform/core'
 import UserController from '@Controllers/UserController'
 import { authUserSchema, storeUserSchema, updateUserSchema } from '@Schemas/UserSchemas'
 import withUserParam from '@Middlewares/withUserParam'
+import withApiKey from '@Middlewares/withApiKey'
 
 const router = express.Router()
 
-router.post('/auth', withBodyValidation(authUserSchema), UserController.auth)
+router.post('/auth', withApiKey, withBodyValidation(authUserSchema), UserController.auth)
 
-router.post('/users', withBodyValidation(storeUserSchema), UserController.store)
-router.get('/users', UserController.index)
-router.patch('/users/:userId', withUserParam, withBodyValidation(updateUserSchema), UserController.update)
-router.delete('/users/:userId', withUserParam, UserController.remove)
-router.get('/users/:userId', withUserParam, UserController.show)
-router.post('/users/:userId/rollback', withUserParam, UserController.rollback)
+router.post('/users', withApiKey, withBodyValidation(storeUserSchema), UserController.store)
+router.get('/users', withApiKey, UserController.index)
+router.patch('/users/:userId', withApiKey, withUserParam, withBodyValidation(updateUserSchema), UserController.update)
+router.delete('/users/:userId', withApiKey, withUserParam, UserController.remove)
+router.get('/users/:userId', withApiKey, withUserParam, UserController.show)
+router.post('/users/:userId/rollback', withApiKey, withUserParam, UserController.rollback)
 
 export default router
